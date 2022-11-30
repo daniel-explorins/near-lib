4a4f277655ac47c44392b86444fe306281775701
marc_explorins <marc@explorins.com>
2022-11-30 12:20:57 +0100

    pruebas git

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { API } from './api'
 import { MintbaseGraphql } from './mintbase-graphql'
 import { MintbaseNearWallet } from './mintbase-near-wallet'
 
+// Pruebas git
+
 export {
   API,
   MintbaseNearWallet,
